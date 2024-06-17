beb779b45ddb150dc2fe3b653c217536778c33c6
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-06-17 21:29:00 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import ProfileCard from './ProfileCard/ProfileCard';
+import user from '../json/user.json'
+import statistic from '../json/statistic.json'
+import friends from '../json/friends.json'
+import transactions from '../json/transactions.json'
+import Section from './Section/Section';
+import Statistic from './Statistic/Statistic';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section>
+        
+        <ProfileCard
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section>
+        <Statistic
+          statisticList={statistic}
+        />
+      </Section>
+
+      <Section>
+        <FriendList
+          friends={friends}
+        />
+      </Section>
+
+      <Section>
+        <TransactionHistory
+          items={transactions}
+        />
+      </Section>
+    </>
   );
 };
